e87822a6595cdeca5fd162ff37fb1c43b08f143d
Jean-Pierre Vacher <jpvacher@gmail.com>
2025-12-09 10:58:16 +0100

    mise à jour TD

--- TD_modeles_mixtes/script_chenilles_TD1.R
+++ TD_modeles_mixtes/script_chenilles_TD1.R
@@ -8,52 +8,42 @@
 rm(list=ls()) #pour effacer les données chargées
 
 # CHARGER LES PACKAGES ####
-library(lme4)
-library(ggeffects)
-library(patchwork)
-library(DHARMa)
-library(maptools)
-library(questionr)
-library(ggplot2)
-library(viridis)
-library(sf)
-library(sjPlot)
-library(cowplot)
-library(lme4)
-library(nlme)
-library(mgcv)
-library(ggeffects)
-library(MuMIn)
-library(performance)
-library(lmerTest)
-library(glmmTMB)
-library(see)
-library(visreg)
-library(reshape2)
+library(sf) #pour traiter les données géographiques (SIG)
+library(ggplot2) #pour les figures
+library(viridis) #pour les couleurs vertes dans ggplot2
+library(ggeffects) #pour fonction ggpredict
+library(questionr) #pour odds.ratio function
+library(lme4) #pour glmer
+library(broom.mixed) #pour fonction tidy
+library(DHARMa) # pour fonction testDispersion
+library(sjPlot) #pour fonction plot_model
+library(MuMIn) #pour fonction r.squaredGLMM
 
 
 #-----------------------------------------------------------#
-#### Le modele lin?aire mixte : chenille processionnaire ####
+#### Le modele lineaire mixte : chenille processionnaire ####
 #-----------------------------------------------------------#
 
 # CHARGER LES DONNEES ####
+#getwd() #permet de voir dans quel directory on se trouve
+setwd("TD_modeles_mixtes") #à taper si on n'est pas dans le bon directory
 chenilles <- read.csv2("chenille_processionnaire.csv",row.names=1, encoding="UTF-8")
 chenilles$prop_attaq <- chenilles$nbpinsattac / chenilles$nbpins
 
-## exploration des donn?es
+# EXPLORATION DES DONNEES ####
 
-# carte des r?gions
+## carte des regions ####
 regions <- st_read("regions.geojson")
 
-ggplot(regions)+
+ggplot(regions) +
   geom_sf(aes(fill=jointure_g))+
   geom_point(data=chenilles,mapping=aes(x=longitude,y=latitude,colour=region),size=2)+
   scale_color_viridis(discrete=TRUE) +
   scale_fill_viridis(discrete=TRUE) + 
-  labs(color="R?gions")+ 
+  labs(color="Régions")+ 
   theme_classic()
 
-##### proportion d'arbres infest?s par an
+## graph proportion d'arbres infestes par an ####
 
 ggplot(chenilles) +
   aes(x = factor(annee), 
@@ -61,45 +51,49 @@ ggplot(chenilles) +
       group = placette, 
       color = region) +
   geom_line(show.legend = FALSE) +
-  labs(x = "Ann?es", y = "Proportion de pins infest?s") +
+  labs(x = "Années", y = "Proportion de pins infestés") +
   facet_wrap(~region, ncol = 1) + 
   theme_classic() + 
   theme(axis.text.x = element_text(angle = 90))+
   scale_color_viridis_d()
 
+#Aucune tendance claire ne se dégage à échelle régionale
+#Ce qui nous intéresse, c’est la tendance sur l’ensemble de l’aire d’étude (et non région par région)
+#Ceci d’autant plus que certaines régions sont mieux échantillonnées que d’autres
+#Et que certaines placettes sont mieux échantillonnées que d’autres
 
-# on rescale les ann?es par commodit?
+
+## on rescale les annees par commodite ####
 chenilles$annee_resc <- chenilles$annee-min(chenilles$annee)+1
 
-#### avec un GLM ####
+#GLM ####
 
-# on commence juste sur une r?gion
+## on commence juste sur une region ####
 chenilles.nord <- subset(chenilles,region=="Centre Nord semi-oceanique")
 mod.nord <- glm(cbind(nbpinsattac,nbpins-nbpinsattac)~annee_resc,family=binomial,data=chenilles.nord)
 
 par(mfrow = c(2,2))
 plot(mod.nord)
-
 summary(mod.nord)
-p.nord <- ggpredict(mod.nord,terms = "annee_resc")%>%
-  plot(residuals=T,log.y=T)
-p.nord
 
-# GLM sur toutes les r?gions
+#p.nord <- ggpredict(mod.nord,terms = "annee_resc") %>%
+#  plot(residuals = T, log.y = T)
+#p.nord
+
+## GLM sur toutes les regions ###
 mod0 <- glm(cbind(nbpinsattac,nbpins-nbpinsattac)~annee_resc,family=binomial,data=chenilles)
 
-# r?sidus
+## residus ####
 par(mfrow=c(2,2))
 plot(mod0)
 
-# interpr?tation
+## interpretation ####
 summary(mod0)
-library(questionr)
 odds.ratio(mod0)
 
-# cartographie des r?sidus
+## cartographie des residus ####
 chenilles$res.mod0 <- residuals(mod0)
-p0 <- plot(ggpredict(mod0, terms = c("annee_resc")), residuals = T)
+#p0 <- plot(ggpredict(mod0, terms = c("annee_resc")), residuals = T)
 
 p1a <- ggplot(regions) +
   geom_sf() +
@@ -107,43 +101,59 @@ p1a <- ggplot(regions) +
              mapping = aes(x = longitude, y = latitude, colour = res.mod0),
              size = 2) +
   scale_color_viridis(discrete = F) + 
-  labs(color = "R?sidus du \n GLM binomial") + 
+  labs(color = "Résidus du \n GLM binomial") + 
   theme_classic()
 
 p1b <- ggplot(chenilles) +
   aes(x = placette, y = res.mod0) +
   geom_boxplot() +
-  labs(x = "Placettes", y = "R?sidus du GLM binomial") +
+  labs(x = "Placettes", y = "Résidus du GLM binomial") +
   theme_classic() + 
   theme(axis.text.x = element_text(angle = 90))
 
 p1a + p1b
 
-## mod?le mixte
+# GLMM ####
 
-# GLMM avec effet placettes dans GRECO 
+#On veut une estimation populationnelle de la tendance, tenant compte de la stratification
+#Le but va être de représenter l’emboîtement des strates dans le modèle
+#Pour ça, le préalable est de repérer quel est cet emboîtement : 
+#Les données annuelles sont échantillonnées dans des placettes
+#Les placettes sont réparties aléatoirement dans des régions
+#Les régions sont réparties dans la zone d’étude
+
+
+## GLMM avec effet placettes dans GRECO ####
 mod2 <- glmer(cbind(nbpinsattac,nbpins-nbpinsattac)~annee_resc+(1|region/placette),family=binomial,data=chenilles)
+#On remarque que l'on part de la strate la plus proche de la population vers la strate la plus proche de l'individu
+
 summary(mod2)
 
-# r?sidus
+# residus ####
+
 plot(mod2, type=c("p","smooth"), col.line=1)
 plot(mod2,
      sqrt(abs(resid(.)))~fitted(.),
-     type=c("p","smooth"), col.line=1,xlab="Valeurs pr?dites",ylab="sqrt(abs(r?sidus))")
+     type=c("p","smooth"), col.line=1,xlab="Valeurs prédites",ylab="sqrt(abs(résidus))")
 
-plot(mod2,xlab="Valeurs pr?dites",ylab="R?sidus de Pearson du GLMM")
-qqnorm(scale(residuals(mod2)))
-abline(0,1)
+plot(mod2,xlab="Valeurs prédites",ylab="Résidus de Pearson du GLMM")
+#En théorie, on devrait vérifier les résidus de chaque strate : en pratique, on ne vérifie généralement que ceux de la première
+#L’hétéroscédasticité détectée à droite n’est en réalité liée qu’à une petite vingtaine de points – on laisse tomber
 
-# interpr?tation
+
+#qqnorm(scale(residuals(mod2)))
+#abline(0,1)
+
+## interpretation ####
 summary(mod2)
 
-library(broom.mixed)
+##Odds – ratios (IC de Wald, approximatif mais rapide) ####
 tidy(mod2,conf.int=TRUE,exponentiate=TRUE,effects="fixed",conf.method="Wald")
+
+##Odds – ratios (IC par profilage, plus correct mais lent) ####
 tidy(mod2,conf.int=TRUE,exponentiate=TRUE,effects="fixed",conf.method="profile")
 
-# surdispersion
-library(DHARMa)
+## surdispersion ####
 simulationOutput <- simulateResiduals(fittedModel = mod2)
 testDispersion(simulationOutput)
 
@@ -152,9 +162,9 @@ testDispersion(simulationOutput,alternative="greater")
 testDispersion(simulationOutput, alternative = "less", plot = FALSE) # seulement sous-dispersion
 testDispersion(simulationOutput, alternative = "greater", plot = FALSE) # seulement surdispersion
 
-# graphiques de r?sultats : effets fixes
+## graphiques de résultats : effets fixes ####
 p3 <- plot(ggpredict(mod2,terms="annee_resc"),residuals=T)+
-  labs(x="Ann?es",y="Taux d'infestation",title="")
+  labs(x="Années",y="Taux d'infestation",title="")
 p3
 
 p4 <- plot(ggpredict(mod2,type="re"),residuals=T)
@@ -166,15 +176,13 @@ p5
 p6 <- plot(ggpredict(mod2,terms=c("annee_resc","placette"),type="re"),residuals=T)
 p6
 
-# effets al?atoires
-library(sjPlot)
+## effets aleatoires ####
 plot_model(mod2, type = "re", show.values = TRUE)
 
-# R?
-library(MuMIn)
+## R2 ###
 r.squaredGLMM(mod2)
 
-# figure sur effets al?atoires
+## figure sur effets aleatoires ####
 rpoints <- st_sample(regions, 700) %>% # random points, as a list ...
   st_sf() %>%  # ... to data frame ...
   st_transform(4326)  # ... and a metric CRS
@@ -191,6 +199,16 @@ ggplot(regions) +
   geom_sf(data = rpoints, pch = 3, col = 'white', alpha = 0.67) +
   scale_color_viridis(discrete = TRUE) +
   scale_fill_viridis(discrete = TRUE) + 
-  labs(color = "R?gions") + 
+  labs(color = "Régions") + 
   theme_classic() +
   theme(legend.position = "none")
+
+#La structure de l’échantillonnage est hiérarchique : on cherche à représenter cette hiérarchie
+#On fait l’hypothèse que notre réseau de placettes est un tirage aléatoire dans une plus grande population de placettes
+#Donc, on ne connaît pas tous les niveaux de la variable placette, mais juste quelques-uns qu’on pense représentatifs de l’ensemble de la population de placettes
+#Idem pour les régions
+
+
+
+#Un intérêt du modèle hiérarchique : la propagation des erreurs
+#L’effet des régions déficitaires est tamponné par les autres régions dans chaque couche stochastique
